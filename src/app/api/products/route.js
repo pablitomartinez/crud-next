bfa101a80b3f4d8f06af8c7db5f3e736e5e267bf
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2023-12-01 18:42:17 -0300

    configuracion de get products

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,1 +1,14 @@
 import { NextResponse } from "next/server";
+
+export const GET = async ()=>{
+    const result = await fetch(process.env.API_URL, {
+        method: "GET",
+        headers: {
+            "Content-type":"application/json"
+        }
+    })
+    const products = await result.json();
+    return NextResponse.json({data: products})
+};
+
+
